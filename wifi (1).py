360864bd080c15c7bd5d89ec722a13e8f79b1a70
FoXhack1 <fox.pclyc@gmail.com>
2024-10-19 02:15:52 +0200

    Update wifi.py

--- wifi (1).py
+++ wifi (1).py
@@ -4,8 +4,11 @@ import json
 networks = []
 
 def write_networks():
-    with open("wifi_networks.json","r") as f:
-        f.write(json.dumps(networks))
+    # Open the file in write mode
+    with open("wifi_networks.json", "w") as f:
+        # Write the JSON data to the file
+        json.dump(networks, f, indent=4)  # Use json.dump instead of json.dumps
+
 def WifiEnumeration(packet):
     if packet.haslayer(Dot11Beacon):
         bssid = packet[Dot11].addr2
@@ -15,12 +18,13 @@ def WifiEnumeration(packet):
         channel = stats.get("channel")
         crypto = stats.get("crypto")
 
+        # Check if crypto is a list or set, convert to list if necessary
+        if isinstance(crypto, set):
+            crypto = list(crypto)
+
         if "WPA/PSK" in crypto or "WPA2/PSK" in crypto:
-            data = {"ssid":ssid, "bssid":bssid, "channel":channel, "crypto": crypto}
+            data = {"ssid": ssid, "bssid": bssid, "channel": channel, "crypto": crypto}
             networks.append(data)
-
-
-
 
 if __name__ == "__main__":
     sniff(prn=WifiEnumeration, iface="wlan1", timeout=5)
